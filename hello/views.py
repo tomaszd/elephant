367fb2aa9367b1675436e868d0f935805f0f02f7
Tomasz Dudziec <tomasz.dudziec@redembedded.com>
2020-11-14 22:41:11 +0100

    initial minors

--- hello/views.py
+++ hello/views.py
@@ -1,20 +1,18 @@
 import os
-import requests
-from django.shortcuts import render
+
 from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import Greeting
 
+
 def index(request):
-    times = int(os.environ.get('TIMES',3))
+    times = int(os.environ.get('TIMES', 3))
     return HttpResponse('Hello! ' * times)
 
 
 def db(request):
-
     greeting = Greeting()
     greeting.save()
-
     greetings = Greeting.objects.all()
-
     return render(request, "db.html", {"greetings": greetings})
