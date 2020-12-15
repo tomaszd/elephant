a52eb3cd8e60adb6b23d1c5c4a8aa726a9ba1a28
Tomasz Dudziec <tomasz.dudziec@redembedded.com>
2020-12-15 16:18:07 +0100

    change model field description to be nullable

--- todo/models.py
+++ todo/models.py
@@ -2,14 +2,20 @@
 
 from django.db import models
 
-
 # Create your models here.
+PRIORITY_CHOICES = [
+    ("Low", "Low"),
+    ("Medium", "Medium"),
+    ("High", "High"),
+]
+
 
 # add this
 class Todo(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(null=True, blank=True)
     completed = models.BooleanField(default=False)
+    priority = models.TextField(choices=PRIORITY_CHOICES, default="Low")
 
     def __str__(self):
         return self.title
