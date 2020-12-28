a9944f5d8f3066940c40d12640ea83012e04527f
Tomasz Dudziec <tomasz.dudziec@redembedded.com>
2020-12-28 12:48:15 +0100

    test commit to see changes in badges

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -120,6 +120,8 @@ CORS_ALLOW_CREDENTIALS = True
 # https://github.com/heroku/django-heroku/issues/39
 if 'I_AM_HEROKU' in os.environ:
     # Configure Django App for Heroku.
-    import django_heroku
+    pass
 
-    django_heroku.settings(locals())
+import django_heroku
+
+django_heroku.settings(locals())
